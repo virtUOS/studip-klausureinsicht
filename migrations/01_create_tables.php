a975c50a9b532c3d1231dc7e513b4195060257c3
Ron Lucke <rlucke@uos.de>
2020-06-23 11:36:21 +0100

    update migration

--- migrations/01_create_tables.php
+++ migrations/01_create_tables.php
@@ -20,19 +20,18 @@ class CreateTables extends Migration
         $db = DBManager::get();
 
         $db->exec("CREATE TABLE IF NOT EXISTS `exam_review_folders` (
-          `seminar_id` varchar(32) NOT NULL,
-          `folder_id` varchar(32) DEFAULT NULL,
-          `chdate` int(11) DEFAULT NULL,
-          `mkdate` int(11) DEFAULT NULL,
+          `seminar_id` varchar(32) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+          `folder_id` varchar(32) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
+          `chdate` int(10) unsigned NOT NULL DEFAULT '0',
+          `mkdate` int(10) unsigned NOT NULL DEFAULT '0',
           PRIMARY KEY (`seminar_id`)
         )");
 
         $db->exec("CREATE TABLE IF NOT EXISTS `exam_review_visits` (
-          `id` int(11) NOT NULL AUTO_INCREMENT,
-          `seminar_id` varchar(32) NOT NULL,
-          `user_id` varchar(32) DEFAULT NULL,
-          `mkdate` int(11) DEFAULT NULL,
-          PRIMARY KEY (`id`)
+          `seminar_id` varchar(32) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+          `user_id` varchar(32) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
+          `mkdate` int(10) unsigned NOT NULL DEFAULT '0',
+          PRIMARY KEY (`seminar_id`,`user_id`)
         )");
 
         SimpleORMap::expireTableScheme();
