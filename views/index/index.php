6f294f16b204f9b8a076ad94195c6049499e6441
Ron Lucke <rlucke@uos.de>
2020-06-23 08:52:27 +0100

    add download button

--- views/index/index.php
+++ views/index/index.php
@@ -1,4 +1,6 @@
 <? if ($pdf_url): ?>
+    <?= \Studip\LinkButton::create('Klausur herunterladen', $pdf_url, ['download' => $pdf_url]); ?>
+    <br>
     <iframe src="<?= $pdf_url?>" width="1000" height="1420"/>
 <? else: ?>
     <?= \MessageBox::info('Es wurde für Sie noch keine Klausur hinterlegt'); ?>
